81b4cbee25288204806cef2f7c19930404dca698
“farah-jannat” <farahjannatmithila@gmail.com>
2024-06-30 16:59:04 +0600

    Updated css in nevbar

--- src/components/Nevbar.jsx
+++ src/components/Nevbar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Nevbar = () => {
   return (
-    <nev className = "flex justify-between bg-slate-700 text-white py-3">
+    <nev className = "flex justify-between bg-indigo-900 text-white py-3">
         <div className="logo">
             <span className='font-bold text-xl mx-9'>iTask</span>
         </div>
       <ul className='flex gap-8 mx-9'>
-        <li className='cursor-pointer'>Home</li>
-        <li className='cursor-pointer'>Your Tasks</li>
+        <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
+        <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
       </ul>
     </nev>
   )
