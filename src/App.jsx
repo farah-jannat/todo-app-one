080f31548edd0d470fe9cd5201ba0bf80587fc2a
“farah-jannat” <farahjannatmithila@gmail.com>
2024-06-30 23:38:21 +0600

    Added necessary fuction in app

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,58 @@ import Nevbar from './components/Nevbar'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
+
+
+
+
+  const handleEdit = () =>{
+
+  }
+
+  const handleDelete =()=>{
+
+  }
+  const handleAdd = ()=>{
+    setTodos([...todos, {todo, isCompleted : false}])
+    setTodo(" ")
+    console.log(todos)
+
+  }
+  const handleChange = (e)=>{
+    setTodo(e.target.value)
+
+  }
+
+  
+
+
+
 
   return (
     <>
     <Nevbar/>
       <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]" >
-      <div className="addTodo">
+      <div className="addTodo my-5">
         <h2 className='text-lg font-bold'> Add a Todo</h2>
-        <input type="text" />
-        <button className='bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 text-white rounded-md mx-6'>Add</button>
+        <input onChange={handleChange} value={todo} type="text"  className='w-1/2'/>
+        <button onClick={handleAdd} className='bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 text-white rounded-md mx-6'>Add</button>
       </div>
           <h2 className='text-lg font-bold'> Your Todos</h2>
         <div className="todos">
-        <div className="todo flex">
-          <div className="text"></div>
+          {todos.map(item=>{
+
+       return  <div className="todo flex w-1/4 my-3 justify-between">
+          <div className={item.isCompleted?"":"line-through" }>{item.todo}</div>
           <div className="buttons">
-           <button>Edit</button>
-           <button>Delete</button>
+           <button onClick= {handleEdit} className='bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 text-white rounded-md mx-1'>Edit</button>
+           <button onClick={handleDelete} className='bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 text-white rounded-md mx-1'>Delete</button>
           </div>
         </div>
+      })}
         </div>
+      
        
       </div>
     </>
