199fd60145846dd50e20fb4fc4688c0cf34985a9
“farah-jannat” <farahjannatmithila@gmail.com>
2024-06-30 17:00:02 +0600

    Updated App.js design

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,23 @@ function App() {
   return (
     <>
     <Nevbar/>
-      <div className="container mx-auto" >
-        <div className="bg-red-600">
-          hey iam red
+      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]" >
+      <div className="addTodo">
+        <h2 className='text-lg font-bold'> Add a Todo</h2>
+        <input type="text" />
+        <button className='bg-violet-800 hover:bg-violet-950 p-2 text-sm font-bold py-1 text-white rounded-md mx-6'>Add</button>
+      </div>
+          <h2 className='text-lg font-bold'> Your Todos</h2>
+        <div className="todos">
+        <div className="todo flex">
+          <div className="text"></div>
+          <div className="buttons">
+           <button>Edit</button>
+           <button>Delete</button>
+          </div>
         </div>
+        </div>
+       
       </div>
     </>
   )
